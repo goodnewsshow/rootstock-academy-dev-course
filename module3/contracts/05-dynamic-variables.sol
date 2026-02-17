b9ab1f0d655f8c91ce7f3bc0c97c60af6f2b5799
Pat Blk <goodnewsshow@yahoo.com>
2026-02-16 21:48:41 -0500

    Fix variable declarations and update Solidity version
    Updated Solidity version and fixed variable declarations.

--- module3/contracts/05-dynamic-variables.sol
+++ module3/contracts/05-dynamic-variables.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.28;
 
 contract Cars {
 
@@ -11,8 +13,9 @@ contract Cars {
         address owner;
     }
 
-    ___ public numCars = ___;
-    ____(uint256 => ___) public cars;
+    uint256 public numCars = 0;
+    mapping(uint256 => Car) public cars;  
+    // Fixed: Car not car, no space after mapping
 
     constructor() {
     }
