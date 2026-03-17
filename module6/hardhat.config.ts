ff40b61ad356523e2319eb4db6c47fed8a0d91aa
Pat Blk <goodnewsshow@yahoo.com>
2026-03-17 01:32:19 -0400

    Add files via upload

--- module6/hardhat.config.ts
+++ module6/hardhat.config.ts
@@ -23,9 +23,7 @@ import * as dotenv from "dotenv";
 // Load environment variables from .env file
 dotenv.config();
 
-
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // Hardhat default key #0
-
 
 // 2: Define RPC URLs
 const RSK_TESTNET_RPC = process.env.RSK_TESTNET_RPC || "https://public-node.testnet.rsk.co";
@@ -65,6 +63,10 @@ const config: HardhatUserConfig = {
         //     url: "http://127.0.0.1:8545",
         //     chainId: 31337
         // },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+            chainId: 31337
+        },
 
         // TODO: Configure RSK Testnet
         // rskTestnet: {
@@ -73,6 +75,12 @@ const config: HardhatUserConfig = {
         //     accounts: [PRIVATE_KEY],
         //     gasPrice: 60000000  // 0.06 gwei - RSK uses lower gas prices
         // },
+         rskTestnet: {
+            url: RSK_TESTNET_RPC,
+            chainId: 31,
+            accounts: [PRIVATE_KEY],
+            gasPrice: 60000000  // 0.06 gwei - RSK uses lower gas prices
+        },
 
         // TODO: Configure RSK Mainnet
         // rskMainnet: {
@@ -81,6 +89,12 @@ const config: HardhatUserConfig = {
         //     accounts: [PRIVATE_KEY],
         //     gasPrice: 60000000
         // }
+         rskMainnet: {
+            url: RSK_MAINNET_RPC,
+            chainId: 30,
+            accounts: [PRIVATE_KEY],
+            gasPrice: 60000000
+        }
     },
 
     // ============================================
